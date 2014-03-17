c4f6e621a2dd07e99f3975c99c7ed3dffedf154c
Sascha Eglau <sascha.eglau@gmail.com>
2014-03-17 23:10:06 +0100

    Switched to isotope v2

--- scripts/index.js
+++ scripts/index.js
@@ -2,10 +2,6 @@ $(document).ready(function() {
     $(window).resize(); // TODO: remove this?
 });
 
-$(window).load(function() {
-    $(window).resize();
-});
-
 $(window).scroll(function() {
     // if ($(document).scrollTop() > 10) {
         // $(".conditional-top-nav").fadeIn(700);
@@ -14,87 +10,24 @@ $(window).scroll(function() {
     // }
 });
 
-$(function() {
+(function() {
     var gridWrapper = $('#grid-wrapper');
 
-    gridWrapper.imagesLoaded(function() {
-        gridWrapper.isotope({
-            itemSelector: '.grid-item'
-        });
-    });
+    var imagePreviewer = function() {
+        var current = -1;
+        var isAnimating = false;
+        var $gridItems = gridWrapper.find('div.grid-item > a');
+        var imgItemsCount = $gridItems.length;
 
-    /*
-     * We're creating a grid with 4 columns
-     * Each picture will take up 1, 2, 3 or 4 columns
-     * This function calculates the column width
-     * based on the grid wrapper width
-    */
-    calculateColumnWidth = function() {
-        var width;
-        if (gridWrapper.width() <= 320) {
-            width = Math.floor(gridWrapper.width());
-        } else if (gridWrapper.width() > 320 && gridWrapper.width() <= 640) {
-            width = Math.floor(gridWrapper.width() / 2);
-        } else if (gridWrapper.width() > 640 && gridWrapper.width() <= 960) {
-            width = Math.floor(gridWrapper.width() / 3);
-        } else {
-            width = Math.floor(gridWrapper.width() / 4);
-        }
-        return width;
-    };
-
-    setGridColumnWidth = function() {
-        var unitWidth = calculateColumnWidth() - 0;
-
-        gridWrapper.children('.width1').css({
-            width: unitWidth
-        });
-
-        gridWrapper.children('.width2').css({
-            width: unitWidth * 2
-        });
-
-        gridWrapper.children('.width3').css({
-            width: unitWidth * 3
-        });
-
-        gridWrapper.children('.width4').css({
-            width: unitWidth * 4
-        });
-
-        // get Isotope instance
-        var isotopeInstance = gridWrapper.data('isotope');
-        if(isotopeInstance){
-            isotopeInstance.masonry.columnWidth = unitWidth;
-            gridWrapper.isotope('reLayout');
-        }
-    };
-
-    $(window).resize(function() {
-        $('#grid-wrapper').css('margin-top', $('#bigimage').outerHeight());
-        $('#ib-img-preview').css({
-            width : $(window).width(),
-            height : $(window).height()
-        });
-
-        setGridColumnWidth();
-    });
-
-    Template = (function() {
-        current = -1,
-        $gridItems = gridWrapper.find('div.grid-item > a'),
-        imgItemsCount = $gridItems.length,
-        isAnimating = false,
-
-        init = function() {
+        var init = function() {
             $gridItems.addClass('gridItem');
             $gridItems.bind('click.ibTemplate', function(ev) {
                 openItem($(this));
                 return false;
             });
-        },
+        };
 
-        openItem = function($item) {
+        var openItem = function($item) {
             if (isAnimating){
                 return false;
             }
@@ -104,9 +37,9 @@ $(function() {
                 isAnimating = false;
             });
 
-        },
+        };
 
-        loadImgPreview = function($item, callback) {
+        var loadImgPreview = function($item, callback) {
             var largeSrc = $item.children('img').data('largesrc'),
                 // description     = $item.children('span').text(),
                 largeImageData = {
@@ -167,131 +100,253 @@ $(function() {
                     initImgPreviewEvents();
                 }
             });
-    },
-
-    preloadImage = function(src, callback) {
-        var img = new Image();
-        img.src = src;
-
-        img.onload = function() {
-            callback(img);
         };
-    },
 
-    initImgPreviewEvents = function() {
-        var $preview = $('#ib-img-preview');
-        $preview.find('span.ib-nav-prev').bind('click.ibTemplate', function( event ) {
-            navigate('prev');
-        }).end().find('span.ib-nav-next').bind('click.ibTemplate', function( event ) {
-            navigate('next');
-        }).end().find('span.ib-close').bind('click.ibTemplate', function( event ) {
-            closeImgPreview();
-        });
-        //resizing the window resizes the preview image
-        $(window).bind('resize.ibTemplate', function(event) {
-            var $largeImg   = $preview.children('.ib-preview-img');
-            var dim         = getImageDim($largeImg);
-            $largeImg.css({
-                width   : dim.width,
-                height  : dim.height,
-                left    : dim.left,
-                top     : dim.top
-            })
-        });
-    },
+        var preloadImage = function(src, callback) {
+            var img = new Image();
+            img.src = src;
 
-    // navigate the image items in fullscreen mode
-    navigate = function(dir) {
-        if(isAnimating) return false;
-        isAnimating = true;
-        var $preview = $('#ib-img-preview'),
-            $loading = $preview.find('div.ib-loading-large');
-        $loading.show();
+            img.onload = function() {
+                callback(img);
+            };
+        };
 
-        if (dir === 'next') {
-            (current === imgItemsCount - 1) ? current = 0 : ++current;
-        } else if (dir === 'prev') {
-            (current === 0) ? current = imgItemsCount - 1 : --current;
-        }
+        var initImgPreviewEvents = function() {
+            var $preview = $('#ib-img-preview');
+            $preview.find('span.ib-nav-prev').bind('click.ibTemplate', function( event ) {
+                navigate('prev');
+            }).end().find('span.ib-nav-next').bind('click.ibTemplate', function( event ) {
+                navigate('next');
+            }).end().find('span.ib-close').bind('click.ibTemplate', function( event ) {
+                closeImgPreview();
+            });
+            //resizing the window resizes the preview image
+            $(window).bind('resize.ibTemplate', function(event) {
+                var $largeImg   = $preview.children('.ib-preview-img');
+                var dim         = getImageDim($largeImg);
+                $largeImg.css({
+                    width   : dim.width,
+                    height  : dim.height,
+                    left    : dim.left,
+                    top     : dim.top
+                })
+            });
+        };
 
-        var $item       = $gridItems.eq(current),
-            largeSrc    = $item.children('img').data('largesrc');
-            // description = $item.children('span').text();
+        // navigate the image items in fullscreen mode
+        var navigate = function(dir) {
+            if(isAnimating) return false;
+            isAnimating = true;
+            var $preview = $('#ib-img-preview'),
+                $loading = $preview.find('div.ib-loading-large');
+            $loading.show();
 
-        preloadImage(largeSrc, function(img) {
-            $loading.hide();
-            //get dimentions for the image, based on the windows size
-            var dim = getImageDim(img);
-            $preview.children('img.ib-preview-img')
-                    .attr('src', largeSrc)
-                    .hide()
-                    .css({
-                        width   : dim.width,
-                        height  : dim.height,
-                        left    : dim.left,
-                        top     : dim.top
-                     }).fadeIn(400);
-                    // .end()
-                    // .find('span.ib-preview-descr')
-                    // .text( description );
+            if (dir === 'next') {
+                (current === imgItemsCount - 1) ? current = 0 : current++;
+            } else if (dir === 'prev') {
+                (current === 0) ? current = imgItemsCount - 1 : current--;
+            }
 
-            isAnimating = false;
-        });
-    },
+            var $item       = $gridItems.eq(current),
+                largeSrc    = $item.children('img').data('largesrc');
+                // description = $item.children('span').text();
 
-    closeImgPreview = function() {
-        if( isAnimating ) return false;
-        isAnimating = true;
-
-        var $item = $gridItems.eq(current);
-        $('#ib-img-preview').find('div.ib-nav, span.ib-close')
+            preloadImage(largeSrc, function(img) {
+                $loading.hide();
+                //get dimentions for the image, based on the windows size
+                var dim = getImageDim(img);
+                $preview.children('img.ib-preview-img')
+                        .attr('src', largeSrc)
                         .hide()
-                        .end()
-                        .animate({
-                            height  : $item.height(),
-                            top     : $item.offset().top
-                            }, 500, 'easeOutExpo', function() {
-                            $(this).animate({
-                                width   : $item.width(),
-                                left    : $item.offset().left
-                                }, 400, function() {
-                                    $(this).fadeOut(function() {isAnimating = false;});
-                            } );
-                        });
-        $('body').removeClass('disable-scroll');
-    },
+                        .css({
+                            width   : dim.width,
+                            height  : dim.height,
+                            left    : dim.left,
+                            top     : dim.top
+                         }).fadeIn(400);
+                        // .end()
+                        // .find('span.ib-preview-descr')
+                        // .text( description );
 
-    getImageDim = function(img) {
-        var w_w = $(window).width(),
-            w_h = $(window).height(),
-            r_w = w_h / w_w,
-            i_w = img.naturalWidth,
-            i_h = img.naturalHeight,
-            r_i = i_h / i_w,
-            new_w, new_h,
-            new_left, new_top;
-
-        if (i_w >= i_h) {
-            new_w = Math.floor(w_w);
-            // check if we're above max and reduce accordingly
-            new_h = Math.floor((i_h * w_w) / i_w);
-            // same here and below
-        }
-        else {
-            new_w = Math.floor((i_w * w_h) / i_h);
-            new_h = w_h;
-        }
-
-        return {
-            width   : new_w,
-            height  : new_h,
-            left    : (w_w - new_w) / 2,
-            top     : (w_h - new_h) / 2
+                isAnimating = false;
+            });
         };
+
+        var closeImgPreview = function() {
+            if( isAnimating ) return false;
+            isAnimating = true;
+
+            var $item = $gridItems.eq(current);
+            $('#ib-img-preview').find('div.ib-nav, span.ib-close')
+                            .hide()
+                            .end()
+                            .animate({
+                                height  : $item.height(),
+                                top     : $item.offset().top
+                                }, 500, 'easeOutExpo', function() {
+                                $(this).animate({
+                                    width   : $item.width(),
+                                    left    : $item.offset().left
+                                    }, 400, function() {
+                                        $(this).fadeOut(function() {isAnimating = false;});
+                                } );
+                            });
+            $('body').removeClass('disable-scroll');
+        };
+
+        var getImageDim = function(img) {
+            var w_w = $(window).width(),
+                w_h = $(window).height(),
+                r_w = w_h / w_w,
+                i_w = img.naturalWidth,
+                i_h = img.naturalHeight,
+                r_i = i_h / i_w,
+                new_w, new_h,
+                new_left, new_top;
+
+            if (i_w >= i_h) {
+                new_w = Math.floor(w_w);
+                // check if we're above max and reduce accordingly
+                new_h = Math.floor((i_h * w_w) / i_w);
+                // same here and below
+            }
+            else {
+                new_w = Math.floor((i_w * w_h) / i_h);
+                new_h = w_h;
+            }
+
+            return {
+                width   : new_w,
+                height  : new_h,
+                left    : (w_w - new_w) / 2,
+                top     : (w_h - new_h) / 2
+            };
+        };
+
+        init();
     };
 
-    return { init : init };
-})();
+    $(window).load(function() {
+        $(window).resize();
 
-    Template.init();
-});
+        loadGrid();
+    });
+
+    $(window).resize(function() {
+        $('#grid-wrapper').css('margin-top', $('#bigimage').outerHeight());
+        $('#ib-img-preview').css({
+            width : $(window).width(),
+            height : $(window).height()
+        });
+
+        setGridColumnWidth();
+    });
+
+    loadGrid = function() {
+        gridWrapper.isotope({
+            itemSelector: '.grid-item',
+            layoutMode: 'masonry',
+            masonry: {
+                columnWidth: '.grid-column'
+            }
+        });
+
+        var properties =
+            [
+                {description: "0", class: "width1"},
+                {description: "1", class: "width2"},
+                {description: "2", class: "width1 grid-column"},
+                {description: "3", class: "width1"},
+                {description: "4", class: "width1"},
+                {description: "5", class: "width1"},
+                {description: "6", class: "width1"},
+                {description: "7", class: "width2"},
+                {description: "8", class: "width1"},
+                {description: "9", class: "width1"},
+                {description: "10", class: "width1"},
+                {description: "11", class: "width2"},
+                {description: "12", class: "width2"},
+                {description: "13", class: "width2"},
+                {description: "14", class: "width1"},
+                {description: "15", class: "width1"},
+                {description: "16", class: "width1"},
+                {description: "17", class: "width1"},
+                {description: "18", class: "width1"},
+                {description: "19", class: "width1"},
+                {description: "20", class: "width1"},
+                {description: "21", class: "width2"},
+                {description: "22", class: "width2"},
+                {description: "23", class: "width1"},
+                {description: "24", class: "width1"},
+                {description: "25", class: "width1"},
+                {description: "26", class: "width1"},
+                {description: "27", class: "width1"},
+                {description: "28", class: "width1"},
+                {description: "29", class: "width2"},
+                {description: "30", class: "width2"}
+            ];
+        var appendElements = [];
+        for(var i = 1; i <= 30; i++) {
+            var itemToAppend = $('#gridItemTmpl').tmpl(
+                    {
+                        src: "images/large/" + i + ".jpg",
+                        thumbSrc: "images/thumbs/" + i + ".jpg",
+                        alt: "Image " + i,
+                        description: properties[i].description,
+                        classes: properties[i].class
+                    });
+            appendElements.push($(itemToAppend[0].outerHTML)[0]);
+        }
+
+        gridWrapper.isotope('insert', appendElements);
+        // gridWrapper.isotope('layoutItems');
+        // var evt = document.createEvent('UIEvents');
+        // evt.initUIEvent('resize', true, false,window,0);
+        // window.dispatchEvent(evt);
+        // $(window).resize();
+        imagePreviewer();
+        $(window).resize();
+    };
+
+    /*
+     * We're creating a grid with 4 columns
+     * Each picture will take up 1, 2, 3 or 4 columns
+     * This function calculates the column width
+     * based on the grid wrapper width
+    */
+    calculateColumnWidth = function() {
+        var width;
+        if (gridWrapper.width() <= 320) {
+            width = Math.floor(gridWrapper.width());
+        } else if (gridWrapper.width() > 320 && gridWrapper.width() <= 640) {
+            width = Math.floor(gridWrapper.width() / 2);
+        } else if (gridWrapper.width() > 640 && gridWrapper.width() <= 960) {
+            width = Math.floor(gridWrapper.width() / 3);
+        } else {
+            width = Math.floor(gridWrapper.width() / 4);
+        }
+        return width;
+    };
+
+    setGridColumnWidth = function() {
+        var unitWidth = calculateColumnWidth() - 0;
+
+        gridWrapper.children('.width1').css({
+            width: unitWidth
+        });
+
+        gridWrapper.children('.width2').css({
+            width: unitWidth * 2
+        });
+
+        gridWrapper.children('.width3').css({
+            width: unitWidth * 3
+        });
+
+        gridWrapper.children('.width4').css({
+            width: unitWidth * 4
+        });
+
+       // gridWrapper.isotope('layout');
+    };
+})();
